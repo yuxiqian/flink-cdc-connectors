bae676b210f28cf43c561555735ace7b82750b5f
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2024-11-25 17:42:24 +0800

    Fix PrePartitionOperatorTest since it doesn't depend on SchemaRegistry now
    Signed-off-by: yuxiqian <34335406+yuxiqian@users.noreply.github.com>

--- flink-cdc-runtime/src/test/java/org/apache/flink/cdc/runtime/partitioning/PrePartitionOperatorTest.java
+++ flink-cdc-runtime/src/test/java/org/apache/flink/cdc/runtime/partitioning/PrePartitionOperatorTest.java
@@ -32,6 +32,8 @@ import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /** Unit test for {@link PrePartitionOperator}. */
@@ -93,10 +95,13 @@ class PrePartitionOperatorTest {
                 createTestHarness()) {
             // Initialization
             testHarness.open();
-            testHarness.registerTableSchema(CUSTOMERS, CUSTOMERS_SCHEMA);
 
-            // DataChangeEvent
+            // CreateTableEvent
             PrePartitionOperator operator = testHarness.getOperator();
+            operator.processElement(
+                    new StreamRecord<>(new CreateTableEvent(CUSTOMERS, CUSTOMERS_SCHEMA)));
+
+            // DataChangeEvents
             BinaryRecordDataGenerator recordDataGenerator =
                     new BinaryRecordDataGenerator(((RowType) CUSTOMERS_SCHEMA.toRowDataType()));
             DataChangeEvent eventA =
@@ -111,6 +116,19 @@ class PrePartitionOperatorTest {
                                     new Object[] {2, new BinaryStringData("Bob"), 12345689L}));
             operator.processElement(new StreamRecord<>(eventA));
             operator.processElement(new StreamRecord<>(eventB));
+
+            // CreateTableEvent will be broadcast to all sinks
+            IntStream.range(0, DOWNSTREAM_PARALLELISM)
+                    .forEach(
+                            i ->
+                                    assertThat(testHarness.getOutputRecords().poll())
+                                            .isEqualTo(
+                                                    new StreamRecord<>(
+                                                            new PartitioningEvent(
+                                                                    new CreateTableEvent(
+                                                                            CUSTOMERS,
+                                                                            CUSTOMERS_SCHEMA),
+                                                                    i))));
             assertThat(testHarness.getOutputRecords().poll())
                     .isEqualTo(
                             new StreamRecord<>(
